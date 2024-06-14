93fe69397b227f8dab7104431373e6eca18b8d03
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-14 14:45:06 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -52,3 +52,4 @@ def process_and_save_image(image):
 # Sử dụng hàm process_and_save_image
 image = st.file_uploader(label="Upload your image here", type=["png", "jpg", "jpeg"])
 process_and_save_image(image)
+st.image(saved_image_path)  # Hiển thị ảnh đã lưu
