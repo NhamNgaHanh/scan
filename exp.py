e4c324001985e4f23e495b78fb239c1a268e3646
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-14 10:21:21 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -15,11 +15,11 @@ def main():
         result_text = []  # empty list for results
         for text in result:
             result_text.append(text[1])
-        st.session_state["Hình ảnh 1"] = result_text
+        st.session_state(["Hình ảnh 1"]) = result_text
     else:
         st.write("Upload an Image")
 with st.container():
-    st.write(st.session_state["Hình ảnh 1"])
+    st.write(st.session_state(["Hình ảnh 1"]))
 @st.cache
 def load_model() -> Reader:
     return ocr.Reader(["en"], model_storage_directory=".")
