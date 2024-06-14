d7f4a1f29e76fa08627647be8c856834b978a563
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-14 10:08:33 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -6,30 +6,19 @@ from easyocr import Reader
 
 
 def main():
-    # title
-    st.title("Easy OCR - Extract Text from Images")
-
-    # subtitle
-    st.markdown("## Optical Character Recognition - Using `easyocr`, `streamlit`")
-
-    # image uploader
     image = st.file_uploader(label="Upload your image here", type=["png", "jpg", "jpeg"])
-
     if image is not None:
-
         input_image = Image.open(image)  # read image
         st.image(input_image)  # display image
+        reader = load_model()  # load model
+        result = reader.readtext(np.array(input_image))
 
-        with st.spinner(" AI is at Work! "):
-            reader = load_model()  # load model
-            result = reader.readtext(np.array(input_image))
+        result_text = []  # empty list for results
 
-            result_text = []  # empty list for results
+        for text in result:
+            result_text.append(text[1])
 
-            for text in result:
-                result_text.append(text[1])
-
-            st.write(result_text)
+        st.write(result_text)
     else:
         st.write("Upload an Image")
 
