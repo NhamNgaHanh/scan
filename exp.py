c951848597068dd2ba1c47157767e43699de6bea
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-14 09:11:13 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,22 +1,28 @@
 import streamlit as st
+import easyocr
 from PIL import Image
-import pytesseract
 
 def ocr_image(image):
-    # Sử dụng Tesseract để nhận diện văn bản
-    text = pytesseract.image_to_string(image)
-    return text
+    # Sử dụng EasyOCR để nhận diện văn bản
+    reader = easyocr.Reader(['en']) # Chỉ định ngôn ngữ (ở đây là tiếng Anh)
+    result = reader.readtext(image)
+    return result
 
 def main():
-    st.title("Ứng dụng OCR sử dụng Streamlit")
+    st.title("Ứng dụng OCR sử dụng EasyOCR và Streamlit")
 
     uploaded_image = st.file_uploader("Tải lên hình ảnh", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
         # Đọc hình ảnh từ file đã tải lên
         image = Image.open(uploaded_image)
-        text = ocr_image(image)
-        st.write(text)
+        st.image(image, caption='Hình ảnh được tải lên', use_column_width=True)
+
+        if st.button('Nhận diện văn bản'):
+            # Thực hiện OCR trên hình ảnh và hiển thị kết quả
+            result = ocr_image(image)
+            for detection in result:
+                st.write(detection[1]) # Hiển thị văn bản đã nhận diện
 
 if __name__ == "__main__":
     main()
