fa398901a516d018cb82aa725a3ff926a04fbd83
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-14 16:58:44 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -2,12 +2,11 @@ import easyocr as ocr  # OCR
 import streamlit as st  # Web App
 from PIL import Image  # Image Processing
 import numpy as np  # Image Processing
-from easyocr import Reader
 import os
 import uuid  # Dùng để tạo một ID duy nhất cho mỗi ảnh
 
 @st.cache
-def load_model() -> Reader:
+def load_model() -> ocr.Reader:
     return ocr.Reader(["en"], model_storage_directory=".")
 
 def save_uploaded_file(image):
@@ -25,9 +24,6 @@ def resize_image(image, target_size=(300, 300)):
     return resized_image
 
 def process(image):
-    saved_image_path = None
-    HA1 = None
-
     if image is not None:
         input_image = Image.open(image)  # read image
         reader = load_model()  # load model
@@ -43,19 +39,18 @@ def process(image):
                 # Kiểm tra nếu ký tự là một ký tự chữ cái hoặc số
                 letters_only = ''.join(c for i, c in enumerate(line) if i < 4 or (c.isalpha() or c.isdigit() or c.isspace()))
                 st.write(letters_only)
-    return letters_only
-    def save_image(image):
-        input_image = Image.open(image)  # read image
-        # Lưu và hiển thị ảnh đã xử lý
-        resized_image = resize_image(input_image, target_size=(300, 300))
-        saved_image_path = save_uploaded_file(resized_image)
-        st.write("Tên của ảnh:", os.path.basename(saved_image_path))
-        st.image(saved_image_path)  # Hiển thị ảnh đã lưu
         return letters_only
 
+def save_image(image):
+    input_image = Image.open(image)  # read image
+    # Lưu và hiển thị ảnh đã xử lý
+    resized_image = resize_image(input_image, target_size=(300, 300))
+    saved_image_path = save_uploaded_file(resized_image)
+    st.write("Tên của ảnh:", os.path.basename(saved_image_path))
+    st.image(saved_image_path)  # Hiển thị ảnh đã lưu
 
-# Sử dụng hàm process_and_save_image
+# Sử dụng hàm process và save_image
 image = st.file_uploader(label="Upload your image here", type=["png", "jpg", "jpeg"])
-saved_image_path = save_image(image)
-file_img = str("./")+str(saved_image_path)
-st.image(file_img)  # Hiển thị ảnh đã lưu
+if image:
+    letters_only = process(image)
+    save_image(image)
